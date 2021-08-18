c3bfcf820c90bbe30e4bc46007c0a73e17759007
Anggito1234 <67083819+Anggito1234@users.noreply.github.com>
2021-08-18 13:52:02 +0700

    Update akademik_data.php

--- app/Views/dashboard/akademik_data.php
+++ app/Views/dashboard/akademik_data.php
@@ -53,9 +53,9 @@
 
                     <!-- Page Heading -->
                     <h1 class="h3 mb-2 text-gray-800">Table Data Akademik</h1>
-                    <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
+                    <!-- <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
                         For more information about DataTables, please visit the <a target="_blank"
-                            href="https://datatables.net">official DataTables documentation</a>.</p>
+                            href="https://datatables.net">official DataTables documentation</a>.</p> -->
                     <div class="row">
                     <div class="col-md-6">
                     <button type="submit" class="btn btn-primary mb-2" data-toggle="modal" data-target="#modalLoginForm">Tambah Data Akademik</button>
