ac7b63dd2f4828cdcba8b882d54dac5979064ed6
rendi14 <rendikuswandi042@gmail.com>
2021-08-25 17:56:58 +0700

    update

---
+++ app/Views/main/TOPKR4.php
@@ -0,0 +1,57 @@
+<section id="heroTOPKR4" class="d-flex justify-cntent-center align-items-center">
+    <div class="carousel-item active">
+        <div class="section-title-bgorange ptb-70">
+            <h1>Kenali ,TOPKR4</h1>
+            <h1>Tata Operasi Perakitan Kendaraan Roda 4</h1>
+        </div>
+    </div>
+</section>
+
+
+
+<div class="containerTOPKR2">
+    <div class="judul2TOPKR">
+        <h2>Kenali Lebih Jauh</h2>
+    </div>
+
+    <section id="icon-boxes22" class="icon-boxes22">
+        <div class="row">
+            <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="fade-up">
+                <div class="icon-box">
+                    <div class="section-title">
+                        <h4>Tentang</h4>
+                        <p> deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+                        <div class="btn22">
+                            <a href="/Halaman/tentangTPMO" class="default-btn">Gabung</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="fade-up" data-aos-delay="100">
+                <div class="icon-box">
+
+                    <div class="section-title">
+                        <h4>Visi Misi</h4>
+                        <p class="">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+                        <div class="btn22">
+                            <a href="/Halaman/visimisiTPMO" class="default-btn">Gabung</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="fade-up" data-aos-delay="200">
+                <div class="icon-box">
+                    <div class="section-title">
+                        <h4>Prestasi</h4>
+                        <p>sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                        <div class="btn22">
+                            <a href="" class="default-btn">Gabung</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
--- app/Views/partial/sidebar.php
+++ app/Views/partial/sidebar.php
@@ -1,44 +1,43 @@
+<!-- Page Wrapper -->
+<div id="wrapper">
 
-    <!-- Page Wrapper -->
-    <div id="wrapper">
+    <!-- Sidebar -->
+    <ul class="navbar-nav bg-orange sidebar sidebar-dark accordion" id="accordionSidebar">
 
-        <!-- Sidebar -->
-        <ul class="navbar-nav bg-orange sidebar sidebar-dark accordion" id="accordionSidebar">
+        <!-- Sidebar - Brand -->
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/dashboard">
+            <div class="sidebar-brand-icon">
+                <img src="/gambar/crop.ico" alt="" width="70" height="40">
+            </div>
+            <div class="sidebar-brand-text mx-3">PT.CROP</div>
+        </a>
 
-            <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/dashboard">
-                <div class="sidebar-brand-icon">
-                    <img src="/gambar/crop.ico" alt="" width="70" height="40">
-                </div>
-                <div class="sidebar-brand-text mx-3">PT.CROP</div>
-            </a>
+        <!-- Divider -->
+        <hr class="sidebar-divider my-0">
 
-            <!-- Divider -->
-            <hr class="sidebar-divider my-0">
-
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item">
-                <a class="nav-link" href="/dashboard">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
-            </li>
-            <?php if (session()->get('user_level') === '1'): ?>
+        <!-- Nav Item - Dashboard -->
+        <li class="nav-item">
+            <a class="nav-link" href="/dashboard">
+                <i class="fas fa-fw fa-tachometer-alt"></i>
+                <span>Dashboard</span></a>
+        </li>
+        <?php if (session()->get('user_level') === '1') : ?>
             <li class="nav-item">
                 <a class="nav-link" href="/MenuDashboard">
                     <i class="fas fa-fw fa-clipboard-list"></i>
                     <span>Halaman Dinamis Admin</span></a>
             </li>
-            <?php else: ?>
+        <?php else : ?>
 
-            <?php endif?>            
-            <!-- Divider -->
-            <hr class="sidebar-divider">
+        <?php endif ?>
+        <!-- Divider -->
+        <hr class="sidebar-divider">
 
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Data 
-            </div>
-            <!-- <li class="nav-item">
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            Data
+        </div>
+        <!-- <li class="nav-item">
                 <a class="nav-link" href="/dashboard/dataDosen">
                     <i class="fas fa-fw fa-users"></i>
                     <span>Data Dosen</span></a>
@@ -53,20 +52,20 @@
                     <i class="fas fa-fw fa-school"></i>
                     <span>Data Prodi</span></a>
             </li> -->
-            <?php foreach ($sidebar as $s): ?>
-                <?php if (session()->get('user_level') === $s['admin_level_kode']): ?>
+        <?php foreach ($sidebar as $s) : ?>
+            <?php if (session()->get('user_level') === $s['admin_level_kode']) : ?>
                 <li class="nav-item">
-                <a class="nav-link" href="/<?= $s['menu_url'] ?>">
-                    <i class="fas fa-fw <?= $s['menu_deskripsi']?>"></i>
-                    <span><?= $s['menu_nama'] ?></span></a>
+                    <a class="nav-link" href="/<?= $s['menu_url'] ?>">
+                        <i class="fas fa-fw <?= $s['menu_deskripsi'] ?>"></i>
+                        <span><?= $s['menu_nama'] ?></span></a>
                 </li>
-                <?php else: ?>
+            <?php else : ?>
 
-                <?php endif?>
-             <?php endforeach ?>   
+            <?php endif ?>
+        <?php endforeach ?>
 
-            <!-- Nav Item - Pages Collapse Menu -->
-<!--             <li class="nav-item">
+        <!-- Nav Item - Pages Collapse Menu -->
+        <!--             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
                     <i class="fas fa-fw fa-cog"></i>
@@ -81,8 +80,8 @@
                 </div>
             </li> -->
 
-            <!-- Nav Item - Utilities Collapse Menu -->
- <!--            <li class="nav-item">
+        <!-- Nav Item - Utilities Collapse Menu -->
+        <!--            <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
                     aria-expanded="true" aria-controls="collapseUtilities">
                     <i class="fas fa-fw fa-wrench"></i>
@@ -100,8 +99,8 @@
                 </div>
             </li> -->
 
-            <!-- Divider -->
-            <?php if (session()->get('user_level') === '1'): ?>
+        <!-- Divider -->
+        <?php if (session()->get('user_level') === '1') : ?>
             <hr class="sidebar-divider">
 
             <!-- Heading -->
@@ -115,25 +114,24 @@
                     <i class="fas fa-fw fa-user"></i>
                     <span>Data Admin</span></a>
             </li>
-            <?php else: ?>
+        <?php else : ?>
 
-            <?php endif?>
+        <?php endif ?>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider d-none d-md-block">
+        <!-- Divider -->
+        <hr class="sidebar-divider d-none d-md-block">
 
-            <!-- Sidebar Toggler (Sidebar) -->
-            <div class="text-center d-none d-md-inline">
-                <button class="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
+        <!-- Sidebar Toggler (Sidebar) -->
+        <div class="text-center d-none d-md-inline">
+            <button class="rounded-circle border-0" id="sidebarToggle"></button>
+        </div>
 
-            <!-- Sidebar Message -->
-<!--             <div class="sidebar-card d-none d-lg-flex">
+        <!-- Sidebar Message -->
+        <!--             <div class="sidebar-card d-none d-lg-flex">
                 <img class="sidebar-card-illustration mb-2" src="img/undraw_rocket.svg" alt="...">
                 <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
                 <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a>
             </div> -->
 
-        </ul>
-        <!-- End of Sidebar -->
-
+    </ul>
+    <!-- End of Sidebar -->
