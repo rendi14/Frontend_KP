8a40380764362bf30faad89fea89ab5f37f915fb
rendi14 <rendikuswandi042@gmail.com>
2021-09-04 19:09:38 +0700

    Update detailBerita.php

--- app/Views/main/detailBerita.php
+++ app/Views/main/detailBerita.php
@@ -1,12 +1,12 @@
-<div class="container mt-3">
+<div class="container mt-3 pt-100">
     <div class="card shadow mb-4 mt-2">
         <div class="card-header py-3">
             <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
         </div>
         <div class="row" style="padding-bottom: 50px;">
-        <div class="col-12 mt-5">
-        <h3 class="text-center"><?= $berita['berita_judul'];?></h3>
-        </div>    
+            <div class="col-12 mt-5">
+                <h3 class="text-center"><?= $berita['berita_judul']; ?></h3>
+            </div>
             <div class="col-6">
                 <div class="text-center mt-5">
                     <a href="<?= $berita['berita_gambar']; ?>">
