a4ea3e9e66bbdb07dd52865f4ed883845c1308a7
Anggito1234 <67083819+Anggito1234@users.noreply.github.com>
2021-09-08 17:43:34 +0700

    Update dashboard.php

--- app/Views/dashboard/dashboard.php
+++ app/Views/dashboard/dashboard.php
@@ -138,22 +138,32 @@
     <div class="card-header py-3">
         <div class="row">
         <div class="col-12 col-xl-8">
+        <?php if ($album_count === 0) :?>
+        <a class="m-0 font-weight-bold text-primary font-size-link">Album Gallery</a>
+        <?php else : ?>
         <a href="" data-toggle="modal" data-target="#modalAlbumData" class="m-0 font-weight-bold text-primary font-size-link">Album Gallery</a>
+        <?php endif ?>
         </div>
         <div class="col-xl-4">
         <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#modalAlbumForm">Tambah Album
             <i class="fas fa-fw fa-plus"></i>
         </button>
+        <?php if ($album_count === 0) :?>
+        <button type="submit" disabled class="btn btn-primary" data-toggle="modal" data-target="#modalFotoForm">Tambah Foto
+            <i class="fas fa-fw fa-plus"></i>
+        </button>
+        <?php else : ?>
         <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#modalFotoForm">Tambah Foto
             <i class="fas fa-fw fa-plus"></i>
         </button>
+        <?php endif ?>
         </div>    
         </div>
     </div>
     
     <div class="row mt-3">
     <?php if ($album_count === 0) :?>
-    <div class="container text-center">
+    <div class="container text-center mt-2">
     <h1>Album Kosong...</h1>
     </div>
     <?php else : ?>
