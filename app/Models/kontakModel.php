e05c348d0c5f533908b5291e51f5d1eca395f1d9
Dapes <irdapisdewa00@gmail.com>
2021-09-09 14:50:48 +0700

    push waktu

--- app/Models/kontakModel.php
+++ app/Models/kontakModel.php
@@ -9,6 +9,7 @@ class kontakModel extends Model
     protected $table = 'akti_contact';
     protected $primaryKey = 'contact_id';
     protected $allowedFields = ['contact_pengirim', 'contact_email', 'contact_telp', 'contact_deskripsi', 'contact_waktu'];
+    protected $createdField = 'created_at';
 
     public function getKontak($id)
     {
